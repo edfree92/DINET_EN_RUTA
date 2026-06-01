c952fd0bf76677482faa233e7ee0d9eb288bffcb
edfree92 <edgar.gv9286@gmail.com>
2026-06-01 01:12:28 -0500

    Add files via upload

---
+++ adventures/picking_altura.js
@@ -0,0 +1,4 @@
+window.ADVENTURE_DATA=window.ADVENTURE_DATA||{};
+window.ADVENTURE_DATA.PICKING_ALTURA={key:"PICKING_ALTURA",label:"PICKING ALTURA",ready:true,scenes:[
+{id:"V2_1",bg:"./BASE/V.2.1.jpg"},{id:"V2_2",bg:"./BASE/V.2.2.jpg"},{id:"V2_3",bg:"./BASE/V.2.3.jpg"},{id:"V2_4",bg:"./BASE/V.2.4.jpg"},{id:"V2_5",bg:"./BASE/V.2.5.jpg"},{id:"V2_6",bg:"./BASE/V.2.6.jpg"},{id:"V2_7",bg:"./BASE/V.2.7.jpg"},{id:"V2_8",bg:"./BASE/V.2.8.jpg"},{id:"V2_9",bg:"./BASE/V.2.9.jpg"},{id:"V2_10",bg:"./BASE/V.2.10.jpg"},{id:"V2_11",bg:"./BASE/V.2.11.jpg"},{id:"V2_12",bg:"./BASE/V.2.12.jpg"},{id:"V2_13",bg:"./BASE/V.2.13.jpg"},{id:"V2_14",bg:"./BASE/V.2.14.jpg"},{id:"V2_15",bg:"./BASE/V.2.15.jpg"},{id:"V2_16",bg:"./BASE/V.2.16.jpg"},{id:"V2_17",bg:"./BASE/V.2.17.jpg"}
+]};
---
+++ adventures/picking_primer_nivel.js
@@ -0,0 +1,4 @@
+window.ADVENTURE_DATA=window.ADVENTURE_DATA||{};
+window.ADVENTURE_DATA.PICKING_PRIMER_NIVEL={key:"PICKING_PRIMER_NIVEL",label:"PICKING PRIMER NIVEL",ready:true,scenes:[
+{id:"V3",bg:"./BASE/V3.jpg"},{id:"V4",bg:"./BASE/V4.jpg"},{id:"V5",bg:"./BASE/V5.jpg"},{id:"V6",bg:"./BASE/V6.jpg"},{id:"V7",bg:"./BASE/V7.jpg"},{id:"V8",bg:"./BASE/V8.jpg"},{id:"V9",bg:"./BASE/V9.jpg"},{id:"V10",bg:"./BASE/V10.jpg"},{id:"V11",bg:"./BASE/V11.jpg"},{id:"V12",bg:"./BASE/V12.jpg"},{id:"V13",bg:"./BASE/V13.jpg"},{id:"V14",bg:"./BASE/V14.jpg"},{id:"V15",bg:"./BASE/V15.jpg"},{id:"V16",bg:"./BASE/V16.jpg"},{id:"V17",bg:"./BASE/V17.jpg"},{id:"V18",bg:"./BASE/V18.jpg"},{id:"V19",bg:"./BASE/V19.jpg"}
+]};
