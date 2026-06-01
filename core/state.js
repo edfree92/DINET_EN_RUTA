490697ddbcf457be4693ad5d8fc0444bda534e89
edfree92 <edgar.gv9286@gmail.com>
2026-06-01 02:06:22 -0500

    Delete core/state.js

--- core/state.js
+++
@@ -1,6 +0,0 @@
-const appState = {
-  users: [], questions: [], adventures: {}, selectedUser: null,
-  currentAdventureKey: null, currentSceneIndex: 0,
-  quiz: { active:false, questions:[], currentIndex:0, answers:[], score:0, startedAt:null, finishedAt:null },
-  currentScreen: "START"
-};
