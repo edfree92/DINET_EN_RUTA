8a63d5d027fef09dd7656c06bbc201cc751b6ae7
edfree92 <edgar.gv9286@gmail.com>
2026-06-01 01:14:51 -0500

    Add files via upload

---
+++ core/config.js
@@ -0,0 +1,12 @@
+const APP_CONFIG = {
+  MOCK_MODE: true,
+  NON_BLOCKING: true,
+  ENDPOINT: "",
+  PATHS: {
+    BASE: "./BASE/",
+    DATA: "./data/",
+    QUESTIONS_CSV: "./data/BANCO_PREGUNTAS.csv",
+    USERS_JSON: "./data/usuarios.json"
+  },
+  QUIZ: { QUESTIONS_PER_ATTEMPT: 5, SCORE_PER_CORRECT: 1, MAX_GRADE: 20 }
+};
