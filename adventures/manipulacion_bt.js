2b8082553c1434306a03d34163578cf75910f6e1
edfree92 <edgar.gv9286@gmail.com>
2026-08-24 17:53:58 -0500

    Add MANIPULACION_BT adventure data
    Agrega modulo Manipulacion Big Ticket

---
+++ adventures/manipulacion_bt.js
@@ -0,0 +1,26 @@
+window.ADVENTURE_DATA = window.ADVENTURE_DATA || {};
+
+window.ADVENTURE_DATA.MANIPULACION_BT = {
+  key: "MANIPULACION_BT",
+  label: "MANIPULACIÓN BIG TICKET",
+  ready: true,
+
+  scenes: [
+    { id:"V5_1",  bg:"./BASE/V.5.1.jpg" },
+    { id:"V5_2",  bg:"./BASE/V.5.2.jpg" },
+    { id:"V5_3",  bg:"./BASE/V.5.3.jpg" },
+    { id:"V5_4",  bg:"./BASE/V.5.4.jpg" },
+    { id:"V5_5",  bg:"./BASE/V.5.5.jpg" },
+    { id:"V5_6",  bg:"./BASE/V.5.6.jpg" },
+    { id:"V5_7",  bg:"./BASE/V.5.7.jpg" },
+    { id:"V5_8",  bg:"./BASE/V.5.8.jpg" },
+    { id:"V5_9",  bg:"./BASE/V.5.9.jpg" },
+    { id:"V5_10", bg:"./BASE/V.5.10.jpg" },
+    { id:"V5_11", bg:"./BASE/V.5.11.jpg" },
+    { id:"V5_12", bg:"./BASE/V.5.12.jpg" },
+    { id:"V5_13", bg:"./BASE/V.5.13.jpg" },
+    { id:"V5_14", bg:"./BASE/V.5.14.jpg" },
+    { id:"V5_15", bg:"./BASE/V.5.15.jpg" },
+    { id:"V5_16", bg:"./BASE/V.5.16.jpg" }
+  ]
+};
